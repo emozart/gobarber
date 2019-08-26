a5865fe1bf8ddb8cdf557cde4ae8aeb203aada61
Elton Mozart <elton.mozart@gmail.com>
2019-08-26 19:44:35 -0300

    Retirada de variáveis não usadas

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -23,7 +23,7 @@ User.prototype.checkPassword = function(password) {
   return bcrypt.compare(password, this.password_hash)
 }
 
-User.addHook('beforeSave', async (user, options) => {
+User.addHook('beforeSave', async user => {
   if (user.password) {
     user.password_hash = await bcrypt.hash(user.password, 8)
   }
